82c64cd44e8b3668a6020c3aa3b4c8ff2a1b1799
luftedar <ugurorcun91@gmail.com>
2022-04-19 01:30:09 +0300

    Import Layout Component to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/globals.css';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component pageProps={pageProps} />;
+  return (
+  <Layout>
+    <Component pageProps={pageProps} />
+  </Layout>
+  );
 }
 
 MyApp.propTypes = {
