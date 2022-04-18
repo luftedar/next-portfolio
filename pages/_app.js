b060b65b6829bc57fa40927fbec1e2655590710e
luftedar <ugurorcun91@gmail.com>
2022-04-19 01:05:02 +0300

    Fixed Props Spreading Error

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component pageProps={pageProps} />;
 }
 
 MyApp.propTypes = {
