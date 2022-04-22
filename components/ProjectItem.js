3fa7e15433a4722c3ed382abdd8b04f28ce31fca
luftedar <ugurorcun91@gmail.com>
2022-04-22 16:03:32 +0300

    Add Styles for ProjectItem

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import projectItem from '../styles/Projectitem.module.css';
 
 const ProjectItem = ({project}) => {
   return (
-    <div>
+    <div className={projectItem.card}>
+      <img src={`https://raw.githubusercontent.com/luftedar/${project.name}/master/app_screenshot.png`} alt="" />
       <h3>{project.name.split("-").join(" ").toUpperCase()}</h3>
       <p>{project.url}</p>
       <p>{project.language}</p>
       <p>{project.topics}</p>
-      <img src={`https://raw.githubusercontent.com/luftedar/${project.name}/master/app_screenshot.png`} alt="" />
     </div>
     )
 }
