594b67ff8cfb5ddabf37087efd4996f09537e205
luftedar <ugurorcun91@gmail.com>
2022-04-22 19:11:00 +0300

    Add Contact Page

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <div>
+      <h2>Hey! Let's get in touch</h2>
+        <p> I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
+      </div>
+    </div>
+  )
+}
+
+export default contact
