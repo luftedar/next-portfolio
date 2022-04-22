1c022e309af5c390d6d98a969e0a4c126246065a
luftedar <ugurorcun91@gmail.com>
2022-04-22 16:59:06 +0300

    Update Styles for Cards

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -4,11 +4,15 @@ import projectItem from '../styles/Projectitem.module.css';
 const ProjectItem = ({project}) => {
   return (
     <div className={projectItem.card}>
-      <img src={`https://raw.githubusercontent.com/luftedar/${project.name}/master/app_screenshot.png`} alt="" />
-      <h3>{project.name.split("-").join(" ").toUpperCase()}</h3>
-      <p>{project.url}</p>
-      <p>{project.language}</p>
-      <p>{project.topics}</p>
+      <div>
+        <img src={`https://raw.githubusercontent.com/luftedar/${project.name}/master/app_screenshot.png`} alt="" />
+      </div>
+      <div>
+        <h3>{project.name.split("-").join(" ").toUpperCase()}</h3>
+        {/* <p>{project.url}</p> */}
+        <p>{project.language}</p>
+        <p>{project.topics}</p>
+      </div>
     </div>
     )
 }
--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
+import projectList from '../styles/Projectlist.module.css';
 
 const ProjectList = ({projects}) => {
   return (
-    <div>
+    <main className={projectList.grid}>
       {projects.map(project => 
       <ProjectItem project={project} key={project.id} />
       )}
-    </div>
+    </main>
   )
 }
 
