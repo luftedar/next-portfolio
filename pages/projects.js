d3b38ba2b0d83e8d9dccc4fb759f905043e10bbd
luftedar <ugurorcun91@gmail.com>
2022-04-22 12:58:12 +0300

    Fetch Data From Github API

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-const projects = () => {
+const projects = ({userInfo}) => {
+  console.log(userInfo);
   return (
     <div>projects</div>
   )
 }
+// getServerSideProps
+// getStaticPaths
+export const getStaticProps = async () => {
+  const userInfo = [];
+  await fetch("https://api.github.com/user/repos?per_page=100", {
+    'method': 'GET',
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization': `token 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`,
+    }
+  }).then(res => res.json()).then(data => data.forEach((x) => {
+    if(x.owner.login === 'luftedar') {
+      console.log(x.topics)
+      userInfo.push({
+        url: x.git_url,
+        name: x.name,
+        language: x.language,
+        topics: x.topics
+      })
+    }
+  }));
+  return {
+    props: {
+      userInfo
+    }
+  };
+}
 
-export default projects
+
+export default projects;
