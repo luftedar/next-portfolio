825829b2b1e519f6b6c311209296378b42ae11c9
luftedar <ugurorcun91@gmail.com>
2022-04-25 00:39:36 +0300

    Add Progress Bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import '../styles/libraries/_nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
