5213e3bc7651cbfcaee0a619bab83a8c50a4fdc6
luftedar <ugurorcun91@gmail.com>
2022-04-25 03:27:40 +0300

    Add Active Passive Button Options for NavBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
+import { Router, useRouter } from 'next/router';
 import navbarStyles from '../styles/Navbar.module.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const router = useRouter();
+  const currentRouter = router.pathname;
+  return (
   <nav className={navbarStyles.nav}>
     <ul>
       <li>
         <Link href="/">
-          Home
+          <a className={currentRouter === '/' ? 'active' : 'passive'}>
+            Home
+          </a>
         </Link>
       </li>
       <li>
         <Link href="/projects">
-          Projects
+          <a className={currentRouter === '/projects' ? 'active' : 'passive'}>
+            Projects
+          </a>
         </Link>
       </li>
       <li>
         <Link href="/contact">
-          Contact
+          <a className={currentRouter === '/contact' ? 'active' : 'passive'}>
+            Contact
+          </a>
         </Link>
       </li>
     </ul>
   </nav>
-);
+  )
+};
 
 export default Navbar;
