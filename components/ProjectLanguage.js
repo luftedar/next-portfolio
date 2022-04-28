92dad4bdb47ae778d153711795b3823ef7b364a8
luftedar <ugurorcun91@gmail.com>
2022-04-28 13:42:58 +0300

    Add ProjectLanguage JSX

--- components/ProjectLanguage.js
+++ components/ProjectLanguage.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const ProjectLanguage = () => {
+const ProjectLanguage = ({language}) => {
+  const langColor = {
+    html: '#e34c26',
+    ruby: '#e34c26',
+    javascript: '#f0db4f',
+    plpgsql: '#008bb9',
+  };
+  console.log(language);
   return (
-    <div>ProjectLanguage</div>
+    <div style={`background-color:{${langColor.language}}`}>{language}</div>
   )
 }
 
