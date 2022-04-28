7052b29a3630c8879ad1477550fac906e5ef421a
luftedar <ugurorcun91@gmail.com>
2022-04-28 13:15:35 +0300

    Update Topic Layout

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import projectItem from '../styles/Projectitem.module.css';
 
-const ProjectItem = ({ project }) => (
-  <div className={projectItem.card}>
-    <div>
-      <img src={`https://raw.githubusercontent.com/luftedar/${project.name}/master/app_screenshot.png`} alt="" />
+const ProjectItem = ({ project }) => {
+  const topicItems = project.topics.map((topic,index) => 
+    <span key={index}>{topic}{' '}</span>
+  ) 
+  return(
+    <div className={projectItem.card}>
+      <div>
+        <img src={`https://raw.githubusercontent.com/luftedar/${project.name}/master/app_screenshot.png`} alt="" />
+      </div>
+      <div>
+        <h3>{project.name.split('-').join(' ').toUpperCase()}</h3>
+        <p>{project.language}</p>
+        <div>
+          {topicItems}
+        </div>
+        <a href={project.url} target="_blank">Source</a>
+      </div>
     </div>
-    <div>
-      <h3>{project.name.split('-').join(' ').toUpperCase()}</h3>
-      {/* <p>{project.url}</p> */}
-      <p>{project.language}</p>
-      <p>{project.topics}</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ProjectItem;
